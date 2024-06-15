be204ab5391792be72d7d5d4436cec58af7f07d6
kartik <kartikmore441@gmail.com>
2024-06-15 10:43:25 +0530

    justify code

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.get("/", (req,res)=>{
 });
 
 //Index route
-app.get("/listing" , async(req,res) =>{
+app.get("/listings" , async(req,res) =>{
     const allListing =  await Listing.find({});
     res.render("./Listings/index.ejs" , {allListing});
 });
@@ -54,13 +54,12 @@ app.get("/listings/:id", async (req, res) => {
 
   
 //Create Route
-app.post("/listings", 
-wrapAsync(async (req, res, next) => {
+app.post("/listings", async(req, res, next) => {
     const newListing = new Listing(req.body.listing);
     await newListing.save();
-    res.redirect("/listing");
+    res.redirect("/listings");
   })
-);
+
 
 
 //Edit Route
@@ -82,7 +81,7 @@ app.get("/listings/:id/edit", async (req, res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
-    res.redirect("/listing");
+    res.redirect("/listings");
   });
 
 // app.get("/testListing",async(req,res)=>{
